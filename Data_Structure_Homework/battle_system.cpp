a85467ff7c03bcdbf065b2f295626f025db7f986
Lin2879 <36335139+Lin2879@users.noreply.github.com>
2018-04-08 21:29:11 +0800

    完成explore_sys和与之相关的interact_sys部分
    准备写一些接口。interact_sys中，e_event的select_from_card_pool和select_from_artifact需要完善。

---
+++ Data_Structure_Homework/battle_system.cpp
@@ -0,0 +1,148 @@
+#include "battle_system.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
+using std::size_t;
+using namespace battle_action_type;
+void battle_system::update()
+{
+	for (auto& i : from_interacting)
+	{
+
+	}
+}
+
+bool battle_system::send_message(info_to_battle_sys input)
+{
+	battle_system::interpret_message(input);
+	//send back something afterwards
+}
+
+bool battle_system::interpret_message(info_to_battle_sys input)
+{
+	for (size_t i = input.action_set.size() - 1; i >= 0; --i)
+	{
+		switch (input.action_set[i].action_id)
+		{
+		case battle_action_type::CALLING_ACTION:
+		{
+			break;
+		}
+		case battle_action_type::PERFORMING_ACTION:
+		{
+			break;
+		}
+		default:
+			break;
+		}
+	}
+	delete &input;
+}
+
+info* battle_system::create_message()
+{
+
+}
+
+void battle_system::process()
+{
+	while (!process_stack.empty())
+	{
+		action temp = process_stack.top();
+		process_stack.pop();
+		switch (temp.action_id)
+		{
+		case battle_action_type::CALLING_ACTION:
+		{
+			send_message(temp.caller->calling_action(temp));
+			break;
+		}
+		case battle_action_type::PERFORMING_ACTION:
+		{
+			send_message(temp.listener->performing_action(temp));
+			break;
+		}
+		case KEEP_A_CARD:
+		{
+			data.b_to_i_pipe = info_battle_to_interacting(temp.action_id, temp.value, temp.type);
+			break;
+		}
+		case REMOVE_A_CARD:
+		{
+			data.b_to_i_pipe = info_battle_to_interacting(temp.action_id, temp.value, temp.type);
+			break;
+		}
+		case DISCARD_A_CARD:
+		{
+			data.b_to_i_pipe = info_battle_to_interacting(temp.action_id, temp.value, temp.type);
+			break;
+		}
+		case DRAW_CARDS:
+		{
+			info_to_battle_sys t(temp);
+			for (auto& i : data.player_data.buff_pool)
+			{
+				t = i.on_calling(t);
+			}
+			vector<card>& c_in_hand = data.cards_in_hand;
+			vector<card>& c_deck = data.cards_deck;
+			vector<card>& c_grave = data.cards_grave;
+			for (size_t i = 0; i < t.action_set.begin()->value; ++i)
+			{
+				if (c_deck.empty())
+				{
+					//需要随机洗牌
+					c_deck = std::move(my_random_engine.xipai(std::move(c_grave)));
+				}
+				if (c_deck.size())//抽空了卡组就抽不出东西了
+
+				{
+					c_in_hand.push_back(*(c_deck.end() - 1));
+					c_deck.pop_back();
+				}
+			}
+			break;
+		}
+
+		case P_KEEP_A_CARD:
+		{
+			data.cards_in_hand[temp.value].is_reserve = 1;
+			break;
+		}
+		case P_REMOVE_A_CARD:
+		{
+			vector<card>& c_in_hand = data.cards_in_hand;
+			vector<card>& c_removed = data.cards_in_hand;
+			c_removed.push_back(c_in_hand[temp.value]);
+			c_in_hand.erase(c_in_hand.begin() + temp.value);
+			break;
+		}
+		case P_DISCARD_A_CARD:
+		{
+			vector<card>& c_in_hand = data.cards_in_hand;
+			vector<card>& c_grave = data.cards_grave;
+			c_grave.push_back(c_in_hand[temp.value]);
+			send_message((c_in_hand.end() - 1)->discard());
+			c_in_hand.erase(c_in_hand.begin() + temp.value);
+			break;
+		}
+		case TURN_END://还要调用buff的turn_end
+		{
+			vector<card>& c_in_hand = data.cards_in_hand;
+			vector<card>& c_grave = data.cards_grave;
+			for (auto i = c_in_hand.begin(); i != c_in_hand.end(); ++i)
+			{
+				if (!i->is_reserve)
+				{
+					c_grave.push_back(*i);
+					c_in_hand.erase(i++);
+				}
+			}
+			break;
+		}
+		default:
+			break;
+		}
+		process_stack.pop();
+	}
+}
---
+++ Data_Structure_Homework/message.cpp
@@ -0,0 +1,120 @@
+#include "message.h"
+using namespace std;
+
+info_to_battle_sys::info_to_battle_sys() 
+	:info(), action_set()
+{
+}
+
+info_to_battle_sys::info_to_battle_sys(action ichange)
+	:info()
+{
+	action_set.push_back(ichange);
+}
+
+void info_to_battle_sys::append(info_to_battle_sys t)
+{
+	for (auto &i : t.action_set)
+	{
+		action_set.push_back(i);
+	}
+}
+
+info::info()
+{
+}
+
+action::action(std::size_t id) :action_id(id), caller(nullptr), listener(nullptr), type(MEANINGLESS_VALUE), value(MEANINGLESS_VALUE)
+{
+}
+
+action::action(std::size_t id, std::size_t ttype, std::size_t tvalue)
+	: action_id(id), caller(nullptr), listener(nullptr), type(ttype), value(tvalue)
+{
+}
+
+action::action(std::size_t id, game_entity * tcaller, game_entity * tlistener, std::size_t ttype, std::size_t tvalue)
+	: action_id(id), caller(tcaller), listener(tlistener), type(ttype), value(tvalue)
+{
+}
+
+info_battle_to_interacting::info_battle_to_interacting(std::size_t ttype, std::size_t tnum, bool tis_m)
+	: type(ttype), num(tnum), is_m(tis_m)
+{
+}
+
+info_battle_to_interacting::operator bool()
+{
+	return (type || num || is_m);
+}
+
+void info_battle_to_interacting::clear()
+{
+	type = num = is_m = 0;
+}
+
+info_battle_to_interacting::info_battle_to_interacting()
+{
+
+}
+
+e_action::e_action(std::size_t id, std::size_t tvalue)
+	:action_id(id), value(tvalue)
+{
+}
+
+e_action::e_action(std::size_t id, artifact tatf)
+	:action_id(id), atf(tatf)
+{
+}
+
+e_action::e_action(std::size_t id, card tcard)
+	:action_id(id), selected_card(tcard)
+{
+}
+
+e_action::e_action(explore_selection exp_s)
+	:selected_card(exp_s.selected_card)
+{
+	action_id = exp_s.type;
+	value = exp_s.value;
+	atf = exp_s.atf;
+}
+
+info_to_explore_sys::info_to_explore_sys()
+	:info(), action_set()
+{
+}
+
+info_to_explore_sys::info_to_explore_sys(e_action ichange)
+	: info()
+{
+	action_set.push_back(ichange);
+}
+
+void info_to_explore_sys::append(info_to_explore_sys t)
+{
+	for (auto &i : t.action_set)
+	{
+		action_set.push_back(i);
+	}
+}
+
+info_explore_to_interacting::info_explore_to_interacting()
+{
+}
+
+info_explore_to_interacting::info_explore_to_interacting(std::size_t ttype, std::size_t tnum, bool tis_m)
+	: type(ttype), num(tnum), is_m(tis_m)
+{
+}
+
+info_explore_to_interacting::operator bool()
+{
+	return (type || num || is_m);
+}
+
+void info_explore_to_interacting::clear()
+{
+	type = num = is_m = 0;
+}
